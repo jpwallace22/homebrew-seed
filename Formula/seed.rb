58474e5d81ce83cf7b9d6c8a2d32f368ac7f91f3
GitHub Action <action@github.com>
2025-01-19 21:49:49 +0000

    Update formula to version 0.1.2

--- Formula/seed.rb
+++ Formula/seed.rb
@@ -1,24 +1,24 @@
 class Seed < Formula
   desc "Plant the seeds of your directory tree 🌱"
   homepage "https://github.com/jpwallace22/seed"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jpwallace22/seed/releases/download/v#{version}/seed_#{version}_darwin_arm64.tar.gz"
-      sha256 "dd10a456b3b8fe930a236a68d4eefb8ba08ae79186996c7d6e2570b25f5c2be9" # darwin_arm64
+      sha256 "234657a18cdea46de20601ff3db490290ab51bb8328fb2d626a4c848862e82a4" # darwin_arm64
     else
       url "https://github.com/jpwallace22/seed/releases/download/v#{version}/seed_#{version}_darwin_amd64.tar.gz"
-      sha256 "472c70aaca569b566250c619e8f8b9bd0562b12763e6b587273c4d7b45b60bb2" # darwin_amd64
+      sha256 "faae9cfb100cbb5ba3153971b04a935af76a391d3fc295670a353065516acb0d" # darwin_amd64
     end
   end
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/jpwallace22/seed/releases/download/v#{version}/seed_#{version}_linux_arm64.tar.gz"
-      sha256 "774d243162ac2545a3dd7a7a894348fa5b2fc8d5093e1f563340388a3ac0eebe" # linux_arm64
+      sha256 "594749b65f0b7c758ac38e3d21c89a20f4b4cc16684cd2173067f623a5152bb6" # linux_arm64
     else
       url "https://github.com/jpwallace22/seed/releases/download/v#{version}/seed_#{version}_linux_amd64.tar.gz"
-      sha256 "03a18d694e0b3abc9750308b2d91fc5ad470e9367c1d6953ffe5d9bee1a8dbe8" # linux_amd64
+      sha256 "5072b2a6dbc3ac0b5f59af6d28f1dab73530a63395d12e7958c2289899d5dff2" # linux_amd64
     end
   end
 
